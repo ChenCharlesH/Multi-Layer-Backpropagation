Numerical Approximation Fix + Gradient Fix

main.py
@@ -14,20 +14,20 @@ def main():
     # Load the testing img and labels
     test_images, test_labels = dat.getTestingData(classes, classes, 0, None)
 
-    # Perform our input normalization.
-    train_images = ut.mean_center_pixel(train_images)
-    train_images, vec = ut.kl_expansion_equal_cov(train_images)
-    train_images = train_images
-    test_images = ut.mean_center_pixel(test_images)
-    test_images = np.dot(vec.T, test_images.T).T
-    test_images = test_images
-
-    # Every-day I'm Shuffling
-    ut.permute(train_images, train_labels)
-    ut.permute(test_images, test_labels)
+    # # Perform our input normalization.
+    # train_images = ut.mean_center_pixel(train_images)
+    # train_images, vec = ut.kl_expansion_equal_cov(train_images)
+    # train_images = train_images
+    # test_images = ut.mean_center_pixel(test_images)
+    # test_images = np.dot(vec.T, test_images.T).T
+    # test_images = test_images
+    #
+    # # Every-day I'm Shuffling
+    # ut.permute(train_images, train_labels)
+    # ut.permute(test_images, test_labels)
 
     # initiate 2 layer Neural Network with Softmax outputs and Logistic hidden layer
-    nn = tl.TwoLayerNN(train_images.shape[1], 64, 10, isTanH=True)
+    nn = tl.TwoLayerNN(train_images.shape[1], 64, 10, isTanH=False)
 
     # Train the Neural Networ
     nn.train(train_images, train_labels, test_images, test_labels, iter=100, n0=0.001,T=100, minibatch=128,

neural_util.py
@@ -49,8 +49,6 @@ def showImg(image):
 # Value of cross entropy.
 def cross_entropy(Y,T):
     res = 0
-    # math.log(min(Y))
-    # print math.log(Y.min)
     for x in range(0, T.size):
             res += T[x] * math.log(Y[x]) + ((1 - T[x]) * math.log(1-Y[x]))
     return -res
@@ -59,13 +57,13 @@ def cross_entropy(Y,T):
 def k_entropy(Y, T):
     res = 0
     rows = Y.shape[0]
-
-    for r in range(0, rows):
-        for v in range(0, T.shape[0]):
+    columns = Y.shape[1]
+    for i in range(0, rows):
+        for j in range(0, columns):
             # if 0 for some reason, set it really close
-            if Y[r,v] == 0.0:
-                Y[r,v] = 0.00001
-            res += T[v,r] * math.log(Y[r,v])
+            if Y[i,j] == 0.0:
+                Y[i,j] = 0.00001
+            res += T[i,j] * math.log(Y[i,j])
     return res
 
 def k_avg_entropy(Y, T):
@@ -131,14 +129,14 @@ def permute(images, labels):
     times = labels.shape[0]
     for i in range(0, times * 2):
         v_i = randint(0, times - 1)
-        v_j = randint(0, times - 1) 
+        v_j = randint(0, times - 1)
 
         images[[v_i, v_j]] = images[[v_j, v_i]]
         labels[[v_i, v_j]] = labels[[v_j, v_i]]
 
 # Get mean of each pixel
 def mean_center_pixel(images):
-    m = images.mean(axis=0,keepdims=True, dtype=np.float64) 
+    m = images.mean(axis=0,keepdims=True, dtype=np.float64)
     return images - m
 
 # Do expansion

two_layer_nn.py
@@ -17,8 +17,10 @@ class TwoLayerNN:
     # Constructor
     def __init__(self, n1, n2, n3, isTanH = False):
         # TODO: Randomize the starting weights with N(0,sigma^2)
-        self.W1 = np.random.normal(0,1/(n1+1),size = (n1 + 1, n2))
-        self.W2 = np.random.normal(0,1/(n2+1),size = (n2 + 1, n3))
+        # self.W1 = np.random.normal(0,1/(n1+1),size = (n1 + 1, n2))
+        # self.W2 = np.random.normal(0,1/(n2+1),size = (n2 + 1, n3))
+        self.W1 = np.random.normal(0,0.1,size = (n1 + 1, n2))
+        self.W2 = np.random.normal(0,0.1,size = (n2 + 1, n3))
         self.isTanH = isTanH
         # self.W1 = np.random.randn(n1 + 1, n2)
         # self.W2 = np.random.randn(n2 + 1, n3)
@@ -46,18 +48,31 @@ class TwoLayerNN:
     # Backpropagation step for given images, labels, and learning rate n
     # We can normalize our batch
     # Returns the two weights.
-    def backprop(self, batch_images, batch_labels, n, reg, regNorm):
+    def backprop(self, batch_images, batch_labels, n, reg, regNorm, isNumerical = False):
         #TODO add reg, regNorm
         delta2 = np.subtract(self.run(batch_images), batch_labels)
         if self.isTanH:
             delta1 = np.multiply(1.7159 * (2.0/3) *(np.multiply(self.Z[:, 1:], (self.Z[:, 1:])) + -1),np.matmul(delta2, self.W2.T[:, :-1]))
         else:
-            delta1 = np.multiply(np.multiply(self.Z[:, 1:], (self.Z[:, 1:] + -1)),np.matmul(delta2, self.W2.T[:, :-1]))
-        dir_res2 = n*np.matmul(np.transpose(self.Z),delta2)/batch_labels.shape[0]
-        dir_res1 = n*np.matmul(np.transpose(self.X),delta1)/batch_labels.shape[0]
-        res2 = self.W2 + dir_res2
-        res1 = self.W1 + dir_res1
-        return res1, res2, dir_res1, dir_res2
+            delta1 = np.multiply(np.multiply(self.Z[:, 1:], (1 - self.Z[:, 1:])),np.matmul(delta2, self.W2.T[:,1:]))
+        grad2 = np.matmul(np.transpose(self.Z),delta2)
+        grad1 = np.matmul(np.transpose(self.X),delta1)
+        if isNumerical:
+            agrad1, agrad2 = self.numApprox(batch_images, batch_labels, 0.01)
+            print "Grad1:"
+            print grad1[0:10,0:5]
+            print agrad1[0:10,0:5]
+            print "---------------------------"
+            print "Grad2:"
+            print grad2[0:10,0:5]
+            print agrad2[0:10,0:5]
+            print "---------------------------"
+            print "Difference:"
+            print np.subtract(grad1[0:10,0:5], agrad1[0:10,0:5])
+            print np.subtract(grad2[0:10,0:5], agrad2[0:10,0:5])
+            print "---------------------------"
+        self.W2 = self.W2 + n*grad2/batch_labels.shape[0]
+        self.W1 = self.W1 + n*grad1/batch_labels.shape[0]
 
     # Helper for numApprox
     def assigner(self, x, W):
@@ -66,46 +81,69 @@ class TwoLayerNN:
         else:
             self.W2 = W
 
-    # Function to find numerical approximation.
+    # # Function to find numerical approximation.
+    # def numApprox(self, images, labels, epsilon):
+    #     values = [self.W1, self.W2]
+    #     res = []
+    #     for t in range(0, len(values)):
+    #         W = values[t]
+    #         prev = np.copy(W)
+    #
+    #         # Generate three grids
+    #         eGrid = np.zeros(shape=W.shape)
+    #         aGrid = np.zeros(shape=W.shape)
+    #         sGrid = np.zeros(shape=W.shape)
+    #         eGrid.fill(epsilon)
+    #
+    #         aW = np.add(W, eGrid)
+    #         sW = np.subtract(W, eGrid)
+    #
+    #         # Calculate + and - values
+    #         for i in range(0, W.shape[0]):
+    #             for j in range(0, W.shape[1]):
+    #                 # Set forward
+    #                 W[i, j] = aW[i, j]
+    #                 # Set specific weights
+    #                 self.assigner(t, W)
+    #                 aGrid[i, j] = self.test(images, labels, False)
+    #
+    #                 W[i, j] = sW[i, j]
+    #                 self.assigner(t, W)
+    #                 sGrid[i, j] = self.test(images, labels, False)
+    #
+    #                 # set values back.
+    #                 self.assigner(t, prev)
+    #
+    #         # calculate our gradient values.
+    #         r = np.subtract(aGrid, sGrid) / (2 * epsilon)
+    #         res.append(r)
+    #         print str(t) + " DONE"
+    #     return res
+
     def numApprox(self, images, labels, epsilon):
-        values = [self.W1, self.W2]
-        res = []
-        for t in range(0, len(values)):
-            W = values[t]
-            prev = np.copy(W)
+        grad1 = np.zeros(self.W1.shape)
+        for i in range(0,10):
+            for j in range(0,5):
+                print i,j
+                self.W1[i,j] = self.W1[i,j] + epsilon;
+                Ypos = self.run(images);
+                self.W1[i,j] = self.W1[i,j] - 2*epsilon;
+                Yneg = self.run(images);
+                self.W1[i,j] = self.W1[i,j] + epsilon;
+                grad1[i,j] = (ut.k_entropy(Ypos,labels)-ut.k_entropy(Yneg,labels))/(2*epsilon)
 
-            # Generate three grids
-            eGrid = np.zeros(shape=W.shape)
-            aGrid = np.zeros(shape=W.shape)
-            sGrid = np.zeros(shape=W.shape)
-            eGrid.fill(epsilon)
+        grad2 = np.zeros(self.W2.shape)
+        for i in range(0,10):
+            for j in range(0,5):
+                print i,j
+                self.W2[i,j] = self.W2[i,j] + epsilon;
+                Ypos = self.run(images);
+                self.W2[i,j] = self.W2[i,j] - 2*epsilon;
+                Yneg = self.run(images);
+                self.W2[i,j] = self.W2[i,j] + epsilon;
+                grad2[i,j] = (ut.k_entropy(Ypos,labels)-ut.k_entropy(Yneg,labels))/(2*epsilon)
 
-            aW = np.add(W, eGrid)
-            sW = np.subtract(W, eGrid)
-
-            # Calculate + and - values
-            for i in range(0, W.shape[0]):
-                for j in range(0, W.shape[1]):
-                    # Set forward
-                    W[i, j] = aW[i, j]
-                    # Set specific weights
-                    self.assigner(t, W)
-                    aGrid[i, j] = self.test(images, labels, False)
-
-                    W[i, j] = sW[i, j]
-                    self.assigner(t, W)
-                    sGrid[i, j] = self.test(images, labels, False)
-
-                    # set values back.
-                    self.assigner(t, prev)
-
-            # calculate our gradient values.
-            r = np.subtract(aGrid, sGrid) / (2 * epsilon)
-            res.append(r)
-            print str(t) + " DONE"
-        return res
-
-
+        return grad1,grad2
 
     def train(
         self, train_images, train_labels, test_images, test_labels,
@@ -135,23 +173,7 @@ class TwoLayerNN:
             errorOld = self.test(holdout_images,holdout_labels)
             for m in range(0, int(np.ceil(float(train_images.shape[0])/minibatch))):
                 batch_images,batch_labels = ut.batch(train_images,train_labels, m, minibatch)
-
-                # Returns weights and also the derivatives
-                bw1, bw2, dir1, dir2 = self.backprop(batch_images, batch_labels, n, reg, regNorm)
-                if isNumerical:
-                    nA = self.numApprox(batch_images, batch_labels, 0.01)
-                    print dir1
-                    print nA[0]
-                    print "---------------------------"
-                    print dir2
-                    print nA[1]
-                    print "---------------------------"
-                    print np.subtract(dir1, nA[0])
-                    print np.subtract(dir2, nA[1])
-                    print "---------------------------"
-
-                self.W1 = bw1
-                self.W2 = bw2
+                self.backprop(batch_images, batch_labels, n, reg, regNorm, isNumerical)
 
             errorNew = self.test(holdout_images,holdout_labels)
             if isPlot: