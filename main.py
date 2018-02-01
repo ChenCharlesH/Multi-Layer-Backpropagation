6a2e30be1ae127bc862611af21722abb5ea957f3
Pragjnesh <pchitimi@eng.ucsd.edu>
2018-01-31 23:22:24 -0800

    Three Layer Neural Network

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import neural_util as ut
 import numpy as np
-import two_layer_nn as tl
+import three_layer_nn as tl
 import data as dat
 import matplotlib.pyplot as plt
 
@@ -10,13 +10,13 @@ def main():
 
     # Load the training img and labels
     train_images, train_labels = dat.getTrainingData(classes, classes, 0, None)
-    train_images = train_images[0:100,:]
-    train_labels = train_labels[0:100]
+    # train_images = train_images[0:100,:]
+    # train_labels = train_labels[0:100]
 
     # Load the testing img and labels
     test_images, test_labels = dat.getTestingData(classes, classes, 0, None)
-    test_images = test_images[0:100,:]
-    test_labels = test_labels[0:100]
+    # test_images = test_images[0:100,:]
+    # test_labels = test_labels[0:100]
 
     # # # Perform our input normalization.
     # train_images = ut.mean_center_pixel(train_images)
@@ -29,23 +29,23 @@ def main():
     # test_images = test_images / std
 
     # initiate 2 layer Neural Network with Softmax outputs and Logistic hidden layer
-    nn = tl.TwoLayerNN(train_images.shape[1], 64, 10, isTanH = False, normWeights = False)
+    nn = tl.ThreeLayerNN(train_images.shape[1], 256, 32, 10, isTanH = True, normWeights = True)
 
-    error1 = nn.train(train_images, train_labels, test_images, test_labels, iter=100, n0=.01, T=10, minibatch=128,
-    earlyStop=3, reg=0.0001, regNorm = 2, alpha=0, isPlot = True, isNumerical = False, isShuffle = False)
+    error1 = nn.train(train_images, train_labels, test_images, test_labels, iter=100, n0=.002, T=25, minibatch=128,
+    earlyStop=3, reg=0.0001, regNorm = 2, alpha=0.9, isPlot = True, isNumerical = False, isShuffle = True)
 
-    nn2 = tl.TwoLayerNN(train_images.shape[1], 64, 10, isTanH = False, normWeights = False)
-
-    # Train the Neural Networ
-    error2 = nn2.train(train_images, train_labels, test_images, test_labels, iter=100, n0=.01, T=10, minibatch=128,
-    earlyStop=3, reg=0.0001, regNorm = 2, alpha=0, isPlot = True, isNumerical = False, isShuffle = True)
-
-    plt.plot(1-error1,label = 'Without Shuffling',linewidth=0.8)
-    plt.plot(1-error2, label = 'Shuffling',linewidth=0.8)
-    plt.xlabel("Epochs")
-    plt.ylabel("Accuracy")
-    plt.legend()
-    plt.show()
+    # nn2 = tl.TwoLayerNN(train_images.shape[1], 64, 10, isTanH = True, normWeights = True)
+    #
+    # # Train the Neural Networ
+    # error2 = nn2.train(train_images, train_labels, test_images, test_labels, iter=100, n0=.01, T=100, minibatch=128,
+    # earlyStop=3, reg=0.0001, regNorm = 2, alpha=0.9, isPlot = True, isNumerical = False, isShuffle = True)
+    #
+    # plt.plot(1-error1,label = 'Without Momentum',linewidth=0.8)
+    # plt.plot(1-error2, label = 'With Momentum',linewidth=0.8)
+    # plt.xlabel("Epochs")
+    # plt.ylabel("Accuracy")
+    # plt.legend()
+    # plt.show()
 
     # Test the Neural Network
     print "Error Rate: " + str(100 * nn.test(test_images,test_labels)) + str("%")
--- three_layer_nn.py
+++ three_layer_nn.py
@@ -3,47 +3,59 @@ import neural_util as ut
 import matplotlib.pyplot as plt
 
 # Class to house logistical regression.
-class TwoLayerNN:
+class ThreeLayerNN:
 
     # Weights should be column vectors
     W1 = np.array([])
     W2 = np.array([])
+    W3 = np.array([])
 
     grad1 = np.array([])
     grad2 = np.array([])
+    grad3 = np.array([])
 
     X = np.array([])
-    Z = np.array([])
+    Z1 = np.array([])
+    Z2 = np.array([])
     Y = np.array([])
     isTanH = False
 
     # Constructor
-    def __init__(self, n1, n2, n3, isTanH = False, normWeights = False):
+    def __init__(self, n1, n2, n3, n4, isTanH = False, normWeights = False):
         # TODO: Randomize the starting weights with N(0,sigma^2)
         if normWeights:
             self.W1 = np.random.normal(0,1.0/np.sqrt(n1+1),size = (n1 + 1, n2))
             self.W2 = np.random.normal(0,1.0/np.sqrt(n2+1),size = (n2 + 1, n3))
+            self.W3 = np.random.normal(0,1.0/np.sqrt(n3+1),size = (n3 + 1, n4))
         else:
             self.W1 = np.random.normal(0,0.1,size = (n1 + 1, n2))
             self.W2 = np.random.normal(0,0.1,size = (n2 + 1, n3))
+            self.W2 = np.random.normal(0,0.1,size = (n3 + 1, n4))
         self.grad1 = np.zeros(self.W1.shape)
         self.grad2 = np.zeros(self.W2.shape)
+        self.grad3 = np.zeros(self.W3.shape)
         self.isTanH = isTanH
-        # self.W1 = np.random.randn(n1 + 1, n2)
-        # self.W2 = np.random.randn(n2 + 1, n3)
 
     def run(self, data):
         # Hidden Layer
         self.X = np.copy(data)
-        Aj = np.matmul(self.X, self.W1)
+
+        Aj1 = np.matmul(self.X, self.W1)
         if self.isTanH:
-            self.Z = ut.stanh(Aj)
+            self.Z1 = ut.stanh(Aj1)
         else:
-            self.Z = ut.logistic(Aj)
-        self.Z = ut.pad_ones(self.Z)
+            self.Z1 = ut.logistic(Aj1)
+        self.Z1 = ut.pad_ones(self.Z1)
+
+        Aj2 = np.matmul(self.Z1, self.W2)
+        if self.isTanH:
+            self.Z2 = ut.stanh(Aj2)
+        else:
+            self.Z2 = ut.logistic(Aj2)
+        self.Z2 = ut.pad_ones(self.Z2)
 
         # Output Layer
-        Ak = np.matmul(self.Z, self.W2)
+        Ak = np.matmul(self.Z2, self.W3)
         self.Y = ut.softmax(Ak)
 
         return self.Y
@@ -53,35 +65,41 @@ class TwoLayerNN:
     # Returns the two weights.
     def backprop(self, batch_images, batch_labels, n, alpha, reg, regNorm, isNumerical = False):
         #TODO add reg, regNorm
-        delta2 = np.subtract(self.run(batch_images), batch_labels)
+        delta3 = np.subtract(self.run(batch_images), batch_labels)
         if self.isTanH:
-            #TODO: Fix the gradient for tanh
-            delta1 = np.multiply( (2.0/3.0)/1.7159 * np.multiply(1.7159 - self.Z[:, 1:],1.7159 + self.Z[:, 1:]) ,np.matmul(delta2, self.W2.T[:, 1:]))
+            delta2 = np.multiply( (2.0/3.0)/1.7159 * np.multiply(1.7159 - self.Z2[:, 1:],1.7159 + self.Z2[:, 1:]) ,np.matmul(delta3, self.W3.T[:, 1:]))
         else:
-            delta1 = np.multiply(np.multiply(self.Z[:, 1:],(1 - self.Z[:, 1:])),np.matmul(delta2, self.W2.T[:,1:]))
-        grad2 = np.matmul(np.transpose(self.Z),delta2)
+            delta2 = np.multiply(np.multiply(self.Z2[:, 1:],(1 - self.Z2[:, 1:])),np.matmul(delta3, self.W3.T[:,1:]))
+        if self.isTanH:
+            delta1 = np.multiply( (2.0/3.0)/1.7159 * np.multiply(1.7159 - self.Z1[:, 1:],1.7159 + self.Z1[:, 1:]) ,np.matmul(delta2, self.W2.T[:, 1:]))
+        else:
+            delta1 = np.multiply(np.multiply(self.Z1[:, 1:],(1 - self.Z1[:, 1:])),np.matmul(delta2, self.W2.T[:,1:]))
+        grad3 = np.matmul(np.transpose(self.Z2),delta3)
+        grad2 = np.matmul(np.transpose(self.Z1),delta2)
         grad1 = np.matmul(np.transpose(self.X),delta1)
-        if isNumerical:
-            agrad1, agrad2 = self.numApprox(batch_images, batch_labels, 0.01)
-            print "Grad1:"
-            print grad1[0:10,0:5]
-            print agrad1[0:10,0:5]
-            print "---------------------------"
-            print "Grad2:"
-            print grad2[0:10,0:5]
-            print agrad2[0:10,0:5]
-            print "---------------------------"
-            print "Max Difference Input Bias:"
-            print np.max(np.absolute(np.subtract(grad1[0,0:5], agrad1[0,0:5])))
-            print "Max Difference Output Bias:"
-            print np.max(np.absolute(np.subtract(grad2[0,0:5], agrad2[0,0:5])))
-            print "Max Difference First Layer Weights:"
-            print np.max(np.absolute(np.subtract(grad1[1:10,0:5], agrad1[1:10,0:5])))
-            print "Max Difference Second Layer Weights:"
-            print np.max(np.absolute(np.subtract(grad2[1:10,0:5], agrad2[1:10,0:5])))
-            print "---------------------------"
+        # if isNumerical:
+        #     agrad1, agrad2 = self.numApprox(batch_images, batch_labels, 0.01)
+        #     print "Grad1:"
+        #     print grad1[0:10,0:5]
+        #     print agrad1[0:10,0:5]
+        #     print "---------------------------"
+        #     print "Grad2:"
+        #     print grad2[0:10,0:5]
+        #     print agrad2[0:10,0:5]
+        #     print "---------------------------"
+        #     print "Max Difference Input Bias:"
+        #     print np.max(np.absolute(np.subtract(grad1[0,0:5], agrad1[0,0:5])))
+        #     print "Max Difference Output Bias:"
+        #     print np.max(np.absolute(np.subtract(grad2[0,0:5], agrad2[0,0:5])))
+        #     print "Max Difference First Layer Weights:"
+        #     print np.max(np.absolute(np.subtract(grad1[1:10,0:5], agrad1[1:10,0:5])))
+        #     print "Max Difference Second Layer Weights:"
+        #     print np.max(np.absolute(np.subtract(grad2[1:10,0:5], agrad2[1:10,0:5])))
+        #     print "---------------------------"
+        self.grad3 = alpha*self.grad3 + n*grad3/batch_labels.shape[0]
         self.grad2 = alpha*self.grad2 + n*grad2/batch_labels.shape[0]
         self.grad1 = alpha*self.grad1 + n*grad1/batch_labels.shape[0]
+        self.W3 = self.W3 + self.grad3
         self.W2 = self.W2 + self.grad2
         self.W1 = self.W1 + self.grad1
 
@@ -106,7 +124,17 @@ class TwoLayerNN:
                 self.W2[i,j] = self.W2[i,j] + epsilon
                 grad2[i,j] = (ut.k_entropy(Ypos,labels)-ut.k_entropy(Yneg,labels))/(2*epsilon)
 
-        return grad1,grad2
+        grad3 = np.zeros(self.W3.shape)
+        for i in range(0,10):
+            for j in range(0,5):
+                self.W3[i,j] = self.W3[i,j] + epsilon
+                Ypos = self.run(images)
+                self.W3[i,j] = self.W3[i,j] - 2*epsilon
+                Yneg = self.run(images)
+                self.W3[i,j] = self.W3[i,j] + epsilon
+                grad2[i,j] = (ut.k_entropy(Ypos,labels)-ut.k_entropy(Yneg,labels))/(2*epsilon)
+
+        return grad1,grad2,grad3
 
     def train(
         self, train_images, train_labels, test_images, test_labels,
@@ -118,6 +146,7 @@ class TwoLayerNN:
         minError = 1
         minW1 = self.W1
         minW2 = self.W2
+        minW3 = self.W3
         train_images, train_labels, holdout_images, holdout_labels = ut.get_holdout(train_images, train_labels,0.1)
         train_images = ut.zscore(train_images)
         train_images = ut.pad_ones(train_images)
@@ -151,8 +180,9 @@ class TwoLayerNN:
             # Keep track of Best performance
             if errorNew < minError:
                 minError = errorNew
-                minW1 = self.W1
-                minW2 = self.W2
+                minW1 = np.copy(self.W1)
+                minW2 = np.copy(self.W2)
+                minW3 = np.copy(self.W3)
 
             if errorNew > errorOld:
                 stopCount = stopCount + 1
@@ -163,6 +193,7 @@ class TwoLayerNN:
 
         self.W1 = minW1
         self.W2 = minW2
+        self.W3 = minW3
 
         # if isPlot:
         #     plt.plot(errorTrain,label = 'Training',linewidth=0.8)
